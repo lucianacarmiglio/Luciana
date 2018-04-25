bee6ebe388382130e409bdc5e3214718c3d306f8
7tecnica <7tecnica@Pc11>
2018-04-25 10:07:10 -0300

    Agregue cuadro de dialogo ingresando el apellido y finalizando con un Buenas noches Tambien se puede ingresar la edad y te muestra lo que pusiste

--- Luciana/src/Main.java
+++ Luciana/src/Main.java
@@ -8,32 +8,14 @@ public class Main {
 		// Se puede utilizar la clase JOptionPane para generar una ventanade dialogo
 		String nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
 		JOptionPane.showMessageDialog(null, "Buen dia " + nombre);
-
+		String nombre1 = JOptionPane.showInputDialog("Su apellido: ");
+		JOptionPane.showMessageDialog(null, "Buenas noches " + nombre1);
 		// También se puede utilizar el objeto Scanner para obtener información de la terminal
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingrese un numero : ");
+		System.out.println("Ingrese la edad : ");
 		System.out.println("Ud ingresó: " + sc.nextInt());
 	}
-	public class OperacionesApp {
-		 
-	    public static void main(String[] args) {
-	 
-	        //Declaramos las variables
-	        int num1=10;
-	        int num2=5;
-	 
-	        /*Realizamos las operaciones.
-	         * Tambien lo podemos guardar el resultado en variables. */
-	 
-	        System.out.println("El resultado de la suma es "+(num1+num2));
-	 
-	        System.out.println("El resultado de la resta es "+(num1-num2));
-	 
-	        System.out.println("El resultado de la multiplicación es "+(num1*num2));
-	 
-	        System.out.println("El resultado de la división es "+(num1/num2));
-	    }
-	}
+	
 
 
 }
